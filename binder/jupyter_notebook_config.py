48555b0ea89e1a0cd769e9caa759a25aca1d8a4c
mccullen <mccullen_j@lynchburg.edu>
2024-07-10 20:04:45 -0400

    add abs url

--- binder/jupyter_notebook_config.py
+++ binder/jupyter_notebook_config.py
@@ -3,6 +3,6 @@ import sys
 c.ServerProxy.servers = {
     "test-server": {
         "command": ["python3", "-m", "http.server", "{port}"],
-        "absolute_url": False
+        "absolute_url": True
     }
 }
